661965bf3a3f3d6e6941d3aafa2119c4dd77ceff
wes-rsato <r.sato@weshare.co.jp>
2025-03-04 17:36:53 +0900

    fixed

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -7,6 +7,11 @@ if [ ! -f "/workspace/app/backend/package.json" ]; then
   npm install yarn
   yarn add express cors @prisma/client
   yarn add -D typescript ts-node nodemon @types/node @types/express
+
+  yes | npx prisma migrate dev --name init
+
+  # add cmd to scripts in /workspace/app/backend/package.json
+  sed -i '1s|{|{\n  "scripts": {\n    "dev": "nodemon src/server.ts"\n  },|' /workspace/app/backend/package.json
 fi
 
 # frontend
@@ -20,6 +25,9 @@ if [ ! -f "/workspace/app/frontend/package.json" ]; then
   cd /workspace/app/frontend
   npm install yarn
   yarn install
+
+  # add args to vite cmd in /workspace/app/frontend/package.json
+  sed -i 's|"dev": "vite"|"dev": "vite --host 0.0.0.0"|' /workspace/app/frontend/package.json
 fi
 
 echo "Finished setting up project."
