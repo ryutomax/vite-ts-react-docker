c46b23e7b792f07b3398aee9bd81612d2f0f7aea
wes-rsato <r.sato@weshare.co.jp>
2025-03-06 14:26:46 +0900

    fixed boot settings

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # backend
+echo "Creating backend project..."
+cd /workspace/app/backend
+
 if [ ! -f "/workspace/app/backend/package.json" ]; then
-  echo "Creating backend project..."
-  cd /workspace/app/backend
   npm install yarn
   yarn add express cors @prisma/client
   yarn add -D typescript ts-node nodemon @types/node @types/express
@@ -14,12 +15,21 @@ if [ ! -f "/workspace/app/backend/package.json" ]; then
   sed -i '1s|{|{\n  "scripts": {\n    "dev": "nodemon ./src/server.ts"\n  },|' /workspace/app/backend/package.json
 fi
 
+# node_modules is empty
+if [ -z "$(ls -A /workspace/app/backend/node_modules 2>/dev/null)" ]; then
+  npm install yarn
+  yarn install
+
+  yes | npx prisma migrate dev --name init
+fi
+
 # frontend
 if [ ! -f "/workspace/app/frontend/package.json" ]; then
   echo "Creating frontend project..."
+  
   cd /workspace
   yes | npx create-vite@latest frontend --template react-ts
-  cp -r /workspace/frontend/* /workspace/app/frontend/
+  cp -r /workspace/frontend/* /workspace/app/frontend/ # 中身だけをコピー
   rm -rf /workspace/frontend  # 一時的なディレクトリを削除
 
   cd /workspace/app/frontend
@@ -28,6 +38,13 @@ if [ ! -f "/workspace/app/frontend/package.json" ]; then
 
   # add args to vite cmd in /workspace/app/frontend/package.json
   sed -i 's|"dev": "vite"|"dev": "vite --host 0.0.0.0"|' /workspace/app/frontend/package.json
+fi
+
+# node_modules is empty
+if [ -z "$(ls -A /workspace/app/frontend/node_modules 2>/dev/null)" ]; then
+  cd /workspace/app/frontend
+  npm install yarn
+  yarn install
 fi
 
 echo "Finished setting up project."
